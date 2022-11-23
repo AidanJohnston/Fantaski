24af8802155943e0a07c62c1581f028494936e27
Aidan Johnston <aidanjhnston@gmail.com>
2022-11-22 21:28:38 -0500

    routing init

--- fantaski-app/src/app/app-routing.module.ts
+++ fantaski-app/src/app/app-routing.module.ts
@@ -1,7 +1,50 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth/auth.guard';
+import { NegateAuthGuard } from './guards/auth/negate-auth.guard';
+import { HomeComponent } from './pages/home/home.component';
+import { LeagueComponent } from './pages/league/league.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'login', 
+    component: LoginComponent,
+    canActivate: [NegateAuthGuard]
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+    canActivate: [NegateAuthGuard]
+  },
+  {
+    path: 'profile',
+    children: [
+      {
+        path: ':username',
+        component: ProfileComponent,
+        canActivate: [AuthGuard]
+      }
+    ]
+  },
+  {
+    path: 'league',
+    children: [
+      {
+        path: ':leaguename',
+        component: LeagueComponent,
+        canActivate: [AuthGuard]
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- fantaski-app/src/app/app.module.ts
+++ fantaski-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { LeagueComponent } from './pages/league/league.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    ProfileComponent,
+    LeagueComponent
   ],
   imports: [
     BrowserModule,
