b08a772ae63253e9bba7e0a45a26ef4edfabb89b
Aidan Johnston <aidanjhnston@gmail.com>
2022-11-23 00:16:44 -0500

    basic login page

--- fantaski-app/src/app/pages/login/login.component.ts
+++ fantaski-app/src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +10,23 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(private _authService: AuthService, private _router : Router) { } 
+
+  isLoading = false;
+  isError = false;
+  hide = true;
+
+onSubmit(login: NgForm){
+    this.isLoading = true;
+    this._authService.loginUserWithPassword(login.value['email'], login.value['password']).then(res => {
+      if(res){
+        this.isLoading = false;
+      }
+      else{
+        this.isError = true;
+        this.isLoading = false;
+        login.reset();
+      }
+    });
+  }
 }
