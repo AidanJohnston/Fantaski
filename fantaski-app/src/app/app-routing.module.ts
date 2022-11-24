ec056da97bd5364341432a94eb25ee24ebe4bdac
Aidan Johnston <aidanjhnston@gmail.com>
2022-11-23 23:02:01 -0500

    routing refactor

--- fantaski-app/src/app/app-routing.module.ts
+++ fantaski-app/src/app/app-routing.module.ts
@@ -1,18 +1,82 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LeagueNavbarComponent } from './components/league-navbar/league-navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { AuthGuard } from './guards/auth/auth.guard';
 import { NegateAuthGuard } from './guards/auth/negate-auth.guard';
+import { EventsComponent } from './pages/events/events.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LeagueComponent } from './pages/league/league.component';
 import { LoginComponent } from './pages/login/login.component';
+import { MatchupComponent } from './pages/matchup/matchup.component';
+import { PlayersComponent } from './pages/players/players.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { SearchComponent } from './pages/search/search.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { TeamComponent } from './pages/team/team.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
-    canActivate: [AuthGuard]
+    component: NavbarComponent,
+    children: [
+      {
+        path: '',
+        children: [
+          {
+            path: 'l',
+            children: [
+              {
+                path: ':leaguename',
+                component: LeagueNavbarComponent,
+                children: [
+                  {
+                    path: '',
+                    component: LeagueComponent,
+                    canActivate: [AuthGuard],
+                  },
+                  {
+                    path: 'team',
+                    component: TeamComponent,
+                    canActivate: [AuthGuard]
+                  },
+                  {
+                    path: 'matchup',
+                    component: MatchupComponent,
+                    canActivate: [AuthGuard]
+                  },
+                  {
+                    path: 'players',
+                    component: PlayersComponent,
+                    canActivate: [AuthGuard]
+                  },
+                  {
+                    path: 'events',
+                    component: EventsComponent,
+                    canActivate: [AuthGuard]
+                  }
+                ]
+              }
+            ]
+          },
+        ]
+      },
+      {
+        path: 'u',
+        children: [
+          {
+            path: ':username',
+            component: ProfileComponent,
+            canActivate: [AuthGuard]
+          }
+        ]
+      },
+      {
+        path: 's',
+        component: SearchComponent,
+        canActivate: [AuthGuard]
+      }
+    ]
   },
   {
     path: 'login', 
@@ -23,26 +87,6 @@ const routes: Routes = [
     path: 'signup',
     component: SignupComponent,
     canActivate: [NegateAuthGuard]
-  },
-  {
-    path: 'profile',
-    children: [
-      {
-        path: ':username',
-        component: ProfileComponent,
-        canActivate: [NegateAuthGuard]
-      }
-    ]
-  },
-  {
-    path: 'league',
-    children: [
-      {
-        path: ':leaguename',
-        component: LeagueComponent,
-        canActivate: [AuthGuard]
-      }
-    ]
   }
 ];
 
