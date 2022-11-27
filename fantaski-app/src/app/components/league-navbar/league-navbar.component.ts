6875ebc8c016085b9a3cbe77423b091a99730fd2
Aidan Johnston <aidanjhnston@gmail.com>
2022-11-27 15:16:25 -0500

    league nav more buttons

--- fantaski-app/src/app/components/league-navbar/league-navbar.component.ts
+++ fantaski-app/src/app/components/league-navbar/league-navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatMenuTrigger } from '@angular/material/menu';
+import { AreYouSureComponent } from './are-you-sure/are-you-sure.component';
 
 @Component({
   selector: 'app-league-navbar',
@@ -7,4 +10,15 @@ import { Component } from '@angular/core';
 })
 export class LeagueNavbarComponent {
 
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    // #docregion focus-restoration
+    const dialogRef = this.dialog.open(AreYouSureComponent, {restoreFocus: false});
+
+    // Manually restore focus to the menu trigger since the element that
+    // opens the dialog won't be in the DOM any more when the dialog closes.
+    // dialogRef.afterClosed().subscribe(() => {      } 
+    // #enddocregion focus-restoration
+  }
 }
